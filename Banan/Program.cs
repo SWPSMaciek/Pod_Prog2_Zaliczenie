f29e18775d2a94d91352f3a4fd425114e6ba3967
maciejswps <epic.stiffly721@passmail.net>
2025-07-04 19:00:59 +0200

    Proper impementation of killing multiple enemies

--- Banan/Program.cs
+++ Banan/Program.cs
@@ -10,7 +10,8 @@ int currentLevelIndex = 0; // Set initial level index here
 Level currentLevel = new Level(currentLevelIndex);
 
 Point startingPoint = new Point(16, 16);
-Player hero = new Player("Snake", "@");
+Player hero = new Player("Cowboy", "@");
+hero.isDead = false;
 hero.speed = 1;
 hero.position = startingPoint;
 
@@ -18,15 +19,23 @@ List<Character> characters = new List<Character>();
 characters.Add(hero);
 
 Random rand = new Random();
-NonPlayerCharacter npc = new NonPlayerCharacter("Liquid", "L");
-npc.position = new Point(rand.Next(12, 16), rand.Next(2, 3));
-//NonPlayerCharacter npc2 = new NonPlayerCharacter("Mario", "M");
-//npc2.position = new Point(rand.Next(16, 30), rand.Next(1, 6));
+NonPlayerCharacter npc = new NonPlayerCharacter("Wario", "W");
+npc.position = new Point(rand.Next(11, 22), 2);
+npc.isDead = false; // Ensure npc2 is not dead
+NonPlayerCharacter npc2 = new NonPlayerCharacter("Mario", "M");
+npc2.position = new Point(rand.Next(16, 30), 3);
+NonPlayerCharacter npc3 = new NonPlayerCharacter("Yoshi", "Y");
+npc3.position = new Point(rand.Next(16, 30), 4);
+NonPlayerCharacter npc4 = new NonPlayerCharacter("Toad", "T");
+npc4.position = new Point(rand.Next(16, 30), 5);
+NonPlayerCharacter npc5 = new NonPlayerCharacter("Thwomp ", "H");
+npc5.position = new Point(rand.Next(16, 30), 6);
 
-if (npc.isDead == false)
-{
-    characters.Add(npc);
-}
+characters.Add(npc);
+characters.Add(npc2);
+characters.Add(npc3);
+characters.Add(npc4);
+characters.Add(npc5);
 
 
 currentLevel.Display();
@@ -47,13 +56,53 @@ while (true)
         if (currentLevelIndex < 5)
         {
             currentLevelIndex++;
-            currentLevel = new Level(currentLevelIndex);
-            currentLevel.Display();
             npc.isDead = false;
         }
 
     }
 
+    switch (currentLevelIndex)
+    {
+        case 1:
+            currentLevel = new Level(1);
+            currentLevel.Display();
+            npc.isDead = false;
+            npc2.isDead = false;
+            break;
+        case 2:
+            currentLevel = new Level(2);
+            currentLevel.Display();
+            npc.isDead = false;
+            npc2.isDead = false;
+            npc3.isDead = false;
+            break;
+        case 3:
+            currentLevel = new Level(3);
+            currentLevel.Display();
+            npc.isDead = false;
+            npc2.isDead = false;
+            npc3.isDead = false;
+            npc4.isDead = false;
+            break;
+        case 4:
+            currentLevel = new Level(4);
+            currentLevel.Display();
+            npc.isDead = false;
+            npc2.isDead = false;
+            npc3.isDead = false;
+            npc4.isDead = false;
+            npc5.isDead = false;
+            break;
+        case 5:
+            currentLevel = new Level(5);
+            currentLevel.Display();
+            npc.isDead = true;
+            npc2.isDead = true;
+            npc3.isDead = true;
+            npc4.isDead = true;
+            npc5.isDead = true;
+            break;
+    }
     foreach (Character element in characters)
     {
         element.Display();
